0f32b414afb06146fde59ca5c98590d7b9a0ca07
jiqianqin <164032052@qq.com>
2017-12-08 18:04:19 +0800

    提交hoc

--- src/modules/couter/containers/couter.js
+++ src/modules/couter/containers/couter.js
@@ -1,6 +1,15 @@
 
-import { compose,lifecycle} from "recompose";
+import { compose,lifecycle,withStateHandlers} from "recompose";
 export const hoc = compose(
+    withStateHandlers(
+        {
+            count: 0
+          },
+          {
+            increment: ({ count }) => () => ({ count: count + 1 }),
+            decrement: ({ count }) => () => ({ count: count - 1 })
+          }
+    ),
     lifecycle({
         componentDidMount: function () {
             console.log("竟然进来了？");
